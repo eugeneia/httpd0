f5b92853626d48fd2327b06a5ad48abe67977f03
max <max@mr.gy>
2013-03-30 00:46:10 +0100

    Fixed a bug in HTTP0.RESOURCE-RESPONDER where invalid mtimes for indexes were reported.

--- resource-responder.lisp
+++ resource-responder.lisp
@@ -12,9 +12,17 @@
 
 (in-package :httpd0.resource-responder)
 
+(defun directory-index (path)
+  "Return index for directory at PATH or NIL."
+  (first (directory (merge-pathnames #p"index.*" path))))
+
 (defun locate-resource (request root)
   "Return resource for REQUEST in ROOT."
-  (probe-file (merge-pathnames request root)))
+  (let ((path (probe-file (merge-pathnames request root))))
+    (when path
+      (or (when (directory-pathname-p path)
+	    (directory-index path))
+	  path))))
 
 (defun last-directory (path)
   "Reduce directory of PATH to its right most directory component."
@@ -43,12 +51,9 @@
 
 (defun serve-directory (path write-date)
   "Serve directory at PATH with WRITE-DATE."
-  (let ((indexes (directory (merge-pathnames #p"index.*" path))))
-    (if indexes
-	(serve-file (first indexes) write-date)
-	(let ((listing (directory-listing path)))
-	  (respond-ok ((length listing) *text-mime* write-date)
-	    (write-sequence listing *standard-output*))))))
+  (let ((listing (directory-listing path)))
+    (respond-ok ((length listing) *text-mime* write-date)
+		(write-sequence listing *standard-output*))))
 
 (defun serve (resource if-modified-since)
   "Serve RESOURCE unless it has not been modified since
